0fabfe5f4ea14b4f01c0a35c9f5cd2b6bf27ccda
Mike Lee <mikemunkyu.lee@gmail.com>
2023-04-03 18:38:19 -0400

    Reboot project

--- src/assets/images/index.ts
+++
@@ -1,7 +0,0 @@
-export default {
-  upload: require('./upload.png'),
-  hand: require('./hand.png'),
-  mesh: require('./mesh.png'),
-  rotate: require('./rotate.png'),
-  github: require('./github.png'),
-};
