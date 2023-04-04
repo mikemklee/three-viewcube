c4b432fceaaf222cf41e0e62e00de76d163d5427
Mike Lee <mikemunkyu.lee@gmail.com>
2023-04-03 20:19:59 -0400

    Add three-viewcube

--- demo/src/App.js
+++ demo/src/App.js
@@ -1,20 +1,36 @@
 import { Canvas } from '@react-three/fiber'
 import { OrbitControls } from '@react-three/drei'
 
-import { useThree } from '@react-three/fiber'
+import ViewCubeController from 'three-viewcube';
+
+import { useThree, useFrame, useRef } from '@react-three/fiber';
+
+import { useEffect } from 'react';
 
 import './App.css';
 
-function Foo() {
-  const state = useThree()
-  console.log(state)
+function useViewCubeController() {
+  const ref = useRef()
+  const { camera, gl } = useThree();
+
+  useEffect(() => {
+    const viewCubeController = new ViewCubeController(camera, gl.domElement);
+    console.log('finished setting up view cube controller')
+  }, [camera, gl]);
+}
+
+
+function Setup() {
+  useViewCubeController();
+  console.log('we  here?')
+
 }
 
 function App() {
   return (
     <div id="canvas-container">
       <Canvas>
-        <Foo />
+        <Setup />
         <ambientLight intensity={0.1} />
         <directionalLight color="red" position={[0, 0, 5]} />
         <mesh>
